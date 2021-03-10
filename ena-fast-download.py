01429d82e5e931b70f18ca1347ea810803b60090
Ben Woodcroft <benjwoodcroft@gmail.com>
2021-03-11 01:52:01 +1000

    ena-fast-download.py: Update copyright.

--- ena-fast-download.py
+++ ena-fast-download.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 __author__ = "Ben Woodcroft"
-__copyright__ = "Copyright 2019"
+__copyright__ = "Copyright 2019-2021"
 __credits__ = ["Ben Woodcroft"]
 __license__ = "GPL3+"
 __maintainer__ = "Ben Woodcroft"
-__email__ = "b.woodcroft near uq.edu.au"
+__email__ = "b.woodcroft near qut.edu.au"
 __status__ = "Development"
 
 import argparse
