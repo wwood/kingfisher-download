9bb4131ed7483a62d0e7944aee8eb2b1952171da
Ben Woodcroft <donttrustben@gmail.com>
2021-04-09 16:34:00 +1000

    Bugfix, add tests.

---
+++ test/test_kingfisher.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+#=======================================================================
+# Authors: Ben Woodcroft
+#
+# Unit tests.
+#
+# Copyright
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License.
+# If not, see <http://www.gnu.org/licenses/>.
+#=======================================================================
+
+import unittest
+import os.path
+import tempdir
+import sys
+import io
+
+import extern
+
+sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')]+sys.path
+kingfisher = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','bin','kingfisher')
+
+class Tests(unittest.TestCase):
+    maxDiff = None
+    
+    def test_unpaid_methods(self):
+        cmd_stub = '{} -r SRR12118866 -m'.format(kingfisher)
+        for method in ('aws-http','prefetch'):
+            with tempdir.in_tempdir():
+                extern.run("{} {}".format(cmd_stub,method))
+                self.assertTrue(os.path.getsize('SRR12118866_1.fastq')==21411192)
+                self.assertTrue(os.path.getsize('SRR12118866_2.fastq')==21411192)
+        
+        with tempdir.in_tempdir():
+            extern.run("{} {}".format(cmd_stub,'ena-ascp'))
+            import IPython; IPython.embed()
+            self.assertTrue(os.path.getsize('SRR12118866_1.fastq.gz')==4117481)
+            self.assertTrue(os.path.getsize('SRR12118866_2.fastq.gz')==4945891)
+
+
+if __name__ == "__main__":
+    unittest.main()
