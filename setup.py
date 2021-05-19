8494f5a2652f1c0d896fc250f692e41114d2b897
Rhys Newell <42852939+rhysnewell@users.noreply.github.com>
2021-05-19 14:59:26 +1000

    Add setup.py.
    * added pip install options
    
    * update ignnore list
    
    * remove pyc
    
    * added pip install options
    
    * update ignnore list
    
    * remove pyc
    
    * rebasing
    
    * update licence type in setup
    
    * remove duplicate code

---
+++ setup.py
@@ -0,0 +1,43 @@
+import io
+from os.path import dirname, join
+from setuptools import setup
+
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def get_version(relpath):
+  """Read version info from a file without importing it"""
+  for line in io.open(join(dirname(__file__), relpath), encoding="cp437"):
+    if "__version__" in line:
+      if '"' in line:
+        return line.split('"')[1]
+      elif "'" in line:
+        return line.split("'")[1]
+
+
+setup(
+    name='kingfisher',
+    version=get_version("kingfisher/version.py"),
+    url='https://github.com/wwood/kingfisher-download',
+    license='GPL-3.0',
+    author='Ben Woodcroft',
+    author_email='',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['kingfisher'],
+    package_data={'': [
+            "kingfisher/*",
+                       ]},
+    data_files=[(".", ["README.md", "LICENCE.txt"])],
+    include_package_data=True,
+    install_requires= [
+    ],
+    scripts=['bin/kingfisher'],
+    classifiers=["Topic :: Scientific/Engineering :: Bio-Informatics"],
+)
