5c70c843913d00e1ee45c25ad3b82552be082fb9
Ben Woodcroft <donttrustben@gmail.com>
2021-07-13 10:16:51 +1000

    annotate: Reduce request number, croak on fail.

--- kingfisher/sra_metadata.py
+++ kingfisher/sra_metadata.py
@@ -12,28 +12,27 @@ import pandas as pd
 
 class SraMetadata:
     def efetch_sra_from_accessions(self, accessions):
-        sra_ids = []
-        for accession in accessions:
-            # SRA IDs from the SRP 
-            res = requests.get(
-                url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi", 
-                params={
-                    "db": "sra",
-                    "term": accession,
-                    "tool": "kingfisher", 
-                    "email": "kingfisher@github.com",
-                    "retmax": 1000,
-                    },
-                )
-            root = ET.fromstring(res.text)
-            sra_ids += list([child.text for child in root.iter('Id')])
-            if not sra_ids:
-                raise Exception(
-                    "No SRA samples found in {}"
-                    .format(accessions))
+        # SRA IDs from the SRP 
+        res = requests.get(
+            url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi", 
+            params={
+                "db": "sra",
+                "term": " OR ".join(["{}[accn]".format(a) for a in accessions]),
+                "tool": "kingfisher", 
+                "email": "kingfisher@github.com",
+                "retmax": 1000,
+                },
+            )
+        root = ET.fromstring(res.text)
+        sra_ids = list([c.text for c in root.find('IdList').getchildren()])
+
+        if not sra_ids:
+            raise Exception(
+                "No SRA samples found in {}"
+                .format(accessions))
 
         blocks = []
-        for block in range(0, len(sra_ids), 25):
+        for block in range(0, len(sra_ids), 50):
             res = requests.get(
                 url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi", 
                 params={
@@ -48,6 +47,15 @@ class SraMetadata:
             df = pd.read_csv(StringIO(res.text.strip()))
             blocks.append(df)
 
-        return pd.concat(blocks)
+        metadata = pd.concat(blocks)
+
+        # Ensure all hits are found
+        not_found_accessions = list([a for a in accessions if a not in metadata['Run'].values])
+        if len(not_found_accessions) > 0:
+            raise Exception("Unable to find accession(s): {}".format(not_found_accessions))
+        if len(metadata) != len(accessions):
+            raise Exception("Found discordant number of results during esearch, not sure what is going on")
+
+        return metadata
 
 
